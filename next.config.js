fec83d3d73b558ff786825e9f597f24ec832b65e
m4lyg0s <dc1beeerm8@gmail.com>
2022-06-18 18:45:58 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  basePath: process.env.GITHUB_ACTIONS ? "/portfolio" : "",
+  trailingSlash: true,
+};
