b1ace7f9ae393f5230122794304fc3a653b66c45
m4lyg0s <dc1beeerm8@gmail.com>
2022-06-18 18:58:11 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.GITHUB_PAGES ? '/portfolio' : '',
 }
 
 module.exports = nextConfig
