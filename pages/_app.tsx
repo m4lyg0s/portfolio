54bd758abead31aad143bb61d7e765109147f330
m4lyg0s <dc1beeerm8@gmail.com>
2022-06-19 22:36:25 +0900

    feat : add pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import 'antd/dist/antd.css';
+import Head from 'next/head';
+import { Layout, Menu } from 'antd';
+import { Content, Footer, Header } from 'antd/lib/layout/layout';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout className='layout'>
+      <Header>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        items={new Array(3).fill(null).map((_, index) => {
+          const key = index + 1;
+          return {
+            key,
+            label: `nav ${key}`,
+          };
+        })}
+      />
+      </Header>
+        <Component {...pageProps} />
+      <Footer>Footer</Footer>
+    </Layout>
+  )
 }
 
 export default MyApp
---
+++ pages/components/hoge.tsx
@@ -0,0 +1,8 @@
+import react from 'react';
+
+const Hoge = () => {
+  return(
+    <div>TEST</div>
+  )
+}
+export default Hoge;
